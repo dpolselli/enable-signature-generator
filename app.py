ac59f540fdf55cafe5d3a36ec94b59c67fa117f7
dpolselli <danielle.polselli@enable.com>
2026-04-24 09:16:10 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -140,12 +140,12 @@ with col_preview:
             <table cellspacing="0" cellpadding="0" border="0" style="border-collapse:collapse;">
               <tr>
                 <td style="padding-right:14px;border-right:2px solid #00A74C;vertical-align:middle;">
-                  <img src="data:image/png;base64,{LOGO_B64}" width="128" height="31" alt="Enable" style="display:block;">
+                  <a href="https://www.enable.com" target="_blank" style="display:block;"><img src="data:image/png;base64,{LOGO_B64}" width="128" height="31" alt="Enable" style="display:block;border:0;"></a>
                 </td>
                 <td style="width:14px;"></td>
                 <td style="vertical-align:middle;font-family:Arial,sans-serif;font-size:14px;color:#000;line-height:1.5;">
                   <span style="font-family:Arial,sans-serif;font-size:14px;color:#000;">{name_display}</span><br>
-                  <span style="font-family:Arial,sans-serif;font-size:14px;color:#000;">{jobtitle}, </span><a href="https://enable.com" style="font-family:Arial,sans-serif;font-size:14px;color:#0B8F43;text-decoration:underline;">Enable</a><br>
+                  <span style="font-family:Arial,sans-serif;font-size:14px;color:#000;">{jobtitle}, </span><a href="https://www.enable.com" target="_blank" style="font-family:Arial,sans-serif;font-size:14px;color:#0B8F43;text-decoration:underline;">Enable</a><br>
                   {contact_line}{tel2_line}{street_line}
                 </td>
               </tr>
